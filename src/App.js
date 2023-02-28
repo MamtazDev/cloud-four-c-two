8796b8b14123edbb65e8ade5332f1f8d294261f5
nipaaa <akternipa941@gmail.com>
2023-02-28 12:28:31 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Login from './components/Register/Login';
 
 function App() {
   return (
   <div>
-    
+    <Login/>
   </div>
   );
 }
--- src/components/Register/Login.js
+++ src/components/Register/Login.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Register.css";
+import loginLogo from "../../assets/login_logo.png";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="py-[60px]">
+        
+    <div className="login_container shadow ">
+      <img className="mx-auto mb-[71px]" src={loginLogo} alt="" />
+      <p className="text-center text-[#C9312E] text-[36px] font-[500] mb-[50px]">
+        Login to your account
+      </p>
+    </div>
+    </div>
+  );
 };
 
 export default Login;

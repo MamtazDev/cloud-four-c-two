00c01c8ca7277fb0310ed8ce4dba3bef86e6c30b
nipaaa <akternipa941@gmail.com>
2023-03-01 12:51:54 -0800

    update project details page

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -1,6 +1,8 @@
 import React from "react";
 import BlueButton from "../../utils/BlueButton";
 import girl from "../../assets/girl.png";
+import bob from "../../assets/bob.png";
+import sarah from "../../assets/sarah.png";
 
 const ProjectDetails = () => {
   return (
@@ -80,7 +82,7 @@ const ProjectDetails = () => {
           </div>
         </div>
       </div>
-      {/* session part */}
+      {/* user part */}
       <div>
         <div className="flex items-center justify-between mb-[12px]">
           <p className="text-[16px] font-[500]">Users</p>
@@ -88,8 +90,8 @@ const ProjectDetails = () => {
         </div>
         <div className="border border-[#3853A4] rounded-[5px] pb-[56px]">
           <div className="flex ">
-            <p className="font-[500] w-[210px] pl-[17px] py-[20px]">
-              Sessions 01{" "}
+            <p className="flex items-center gap-[11px] font-[500] w-[210px] pl-[17px] py-[20px]">
+              <img width={36} src={bob} alt="" /> Bob
             </p>
 
             <p className=" session_bg w-[257px] py-[20px] text-center font-[400]">
@@ -100,8 +102,8 @@ const ProjectDetails = () => {
             </p>
           </div>
           <div className="flex text-[16px]">
-            <p className="w-[210px] font-[500] pl-[17px] py-[20px]">
-              Sessions 02{" "}
+            <p className="flex items-center gap-[11px] font-[500] w-[210px] pl-[17px] py-[20px]">
+              <img width={36} src={sarah} alt="" /> Sarah
             </p>
 
             <p className=" session_bg  w-[257px] py-[20px] text-center font-[400]">
@@ -109,6 +111,42 @@ const ProjectDetails = () => {
             </p>
             <p className=" session_bg  w-[257px] py-[20px] text-center font-[400]">
               Change role (ANALYST)
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* recent activity part */}
+      <div className="mb-[22px]">
+        <div className="flex items-center justify-between mb-[12px]">
+          <p className="text-[16px] font-[500]">Recent Activity</p>
+          <BlueButton>Full report</BlueButton>
+        </div>
+        <div className="border border-[#3853A4] rounded-[5px] pb-[56px]">
+          <div className="flex ">
+            <p className="font-[500] w-[210px] pl-[17px] py-[20px]">
+         Session 01
+            </p>
+
+            <p className=" session_bg w-[257px] py-[20px] text-center font-[400]">
+              Remove (ANALYST)
+            </p>
+            <p className=" session_bg w-[257px] py-[20px] text-center font-[400]">
+              Change role (ANALYST)
+            </p>
+          </div>
+          <div className="flex text-[16px]">
+            <p className="w-[210px] font-[500] pl-[17px] py-[20px]">Bob</p>
+            <p className="font-[500] py-[20px]">
+              this is some text log, no buttons should be here
+            </p>
+          </div>
+          <div className="flex text-[16px]">
+            <p className="w-[210px] font-[500] pl-[17px] py-[20px]"></p>
+            <p className="font-[500] py-[20px] mr-[97px]">
+            Remove (ANALYST)
+            </p>
+            <p className="font-[500] py-[20px]">
+            Change role (ANALYST)
             </p>
           </div>
         </div>

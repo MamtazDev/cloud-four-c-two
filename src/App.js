151f785617fab9ab92144239e2885f3b8dc00f35
nipaaa <akternipa941@gmail.com>
2023-03-01 13:08:14 -0800

    update project report

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Account from "./components/Account/Account";
 import Manage from "./components/MangeUser/Manage";
 import Project from "./components/Projects/Project";
 import ProjectDetails from "./components/Projects/ProjectDetails";
+import ProjectReport from "./components/Projects/ProjectReport";
 import Login from "./components/Register/Login";
 import Register from "./components/Register/Register";
 import Sidebar from "./components/shared/Sidebar";
@@ -18,12 +19,11 @@ function App() {
           <Route index element={<Account />}></Route>
           <Route path="project" element={<Project />}></Route>
           <Route path="projectDetails" element={<ProjectDetails />}></Route>
-           <Route path='manage' element={<Manage />}></Route>
-
+          <Route path="projectReport" element={<ProjectReport />}></Route>
+          <Route path="manage" element={<Manage />}></Route>
         </Route>
 
         <Route path="/register" element={<Register />} />
-
       </Routes>
     </div>
   );
---
+++ src/components/Projects/ProjectReport.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BlueButton from "../../utils/BlueButton";
+
+const ProjectReport = () => {
+  return (
+    <div className="p-[60px]">
+      <div className="mb-[82px]">
+        <BlueButton>Back to project</BlueButton>
+      </div>
+      <p className="text-[20px] font-[500] mb-[18px]">Log</p>
+    </div>
+  );
+};
+
+export default ProjectReport;

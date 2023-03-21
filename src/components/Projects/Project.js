c7302a8698bf390f2d2c421eb018630bf2d3d168
MamtazDev <akter.mstmamtaz@gmail.com>
2023-03-21 14:03:43 +0600

    project page modal update

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -14,6 +14,19 @@ import axios from "axios";
 
 const Project = () => {
   const [projects, setProjects] = useState([]);
+  const [base64Image, setBase64Image] = useState("");
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setBase64Image(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
+  // console.log(base64Image);
 
   axios.defaults.withCredentials = true;
   useEffect(() => {
@@ -26,7 +39,37 @@ const Project = () => {
       })
       .then((response) => setProjects(response.data.projects));
   }, []);
-  console.log(projects);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const project_name = e.target.project_name.value;
+    const project_description = e.target.project_description.value;
+    const project_path_to_execute = e.target.project_path_to_execute.value;
+
+    const info = {
+      project_name,
+      file_data: base64Image,
+      project_path_to_execute,
+      project_description,
+      // project_image: base64Image,
+    };
+    console.log(info);
+
+    fetch("https://app.cloud4c2.com/api/project/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(info),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "New project successfully created") {
+          e.target.reset();
+          alert(data.message);
+        }
+      });
+  };
   const handleDelete = () => {
     Swal.fire({
       title: "Are you sure?",
@@ -71,7 +114,74 @@ const Project = () => {
               >
                 ✕
               </label>
-              <CreateProject />
+              {/* <CreateProject /> */}
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-[33px]">
+                    <label className="outfit text-[20px] font-[300] mb-[10px] block">
+                      Project Name <sup className="text-[#C9312E]">*</sup>
+                    </label>
+                    <input
+                      type="text"
+                      className="input input-bordered w-full h-[56px]"
+                      name="project_name"
+                    />
+                  </div>
+                  <div className="mb-[33px]">
+                    <label className="outfit text-[20px] font-[300] mb-[10px] block">
+                      Project Description{" "}
+                      <sup className="text-[#C9312E]">*</sup>
+                    </label>
+                    <textarea
+                      className="input input-bordered w-full"
+                      name="project_description"
+                    ></textarea>
+                  </div>
+                  {/* <div className="mb-[33px]">
+          <label className="outfit text-[20px] font-[300] mb-[10px] block">
+            Project Image <sup className="text-[#C9312E]">*</sup>
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            className="file-input w-full bg-white"
+            onChange={handleImageChange}
+          />
+        </div> */}
+                  <div className="mb-[33px]">
+                    <label className="outfit text-[20px] font-[300] mb-[10px] block">
+                      File Data <sup className="text-[#C9312E]">*</sup>
+                    </label>
+                    <input
+                      type="file"
+                      name="file_data"
+                      accept=".zip,.rar,.7zip"
+                      className="file-input w-full bg-white"
+                      onChange={handleImageChange}
+                    />
+                  </div>
+                  <div className="mb-[33px]">
+                    <label className="outfit text-[20px] font-[300] mb-[10px] block">
+                      Project Path to Execute{" "}
+                      <sup className="text-[#C9312E]">*</sup>
+                    </label>
+                    <input
+                      type="text"
+                      className="input input-bordered w-full h-[56px]"
+                      name="project_path_to_execute"
+                    />
+                  </div>
+
+                  <div className="flex justify-center gap-[30px] mb-[30px]">
+                    <button
+                      type="submit"
+                      className="outfit bg-[#3853A4] p-3 lg:py-[17px] lg:px-[50px] text-white text-[15px] lg:text-[20px] font-[500] rounded-[5px]"
+                    >
+                      Create
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
 
@@ -226,14 +336,16 @@ const Project = () => {
         </label>
 
         {projects.map((project) => (
-          <div  key={project.project_id}>
+          <div key={project.project_id}>
             {/* project (staff) parts */}
             {/* The button to open modal */}
-            <label className="cursor-pointer" htmlFor={"my-modal" + project.project_id}>
+            <label
+              className="cursor-pointer"
+              htmlFor={"my-modal" + project.project_id}
+            >
               {" "}
               <div className="card_box p-[7px]">
                 <div className="img_box">
-              
                   <div>
                     <img
                       width={100}

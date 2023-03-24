8cd8a20a8567ef0d399c1f3b34f8187d00e64af8
mohi14 <mohiuddinkhan1409@gmail.com>
2023-03-24 09:35:00 +0600

    some changes done

--- src/components/Account/EditUser.js
+++ src/components/Account/EditUser.js
@@ -200,7 +200,7 @@ const EditUser = () => {
                 type="submit"
                 className="outfit bg-[#3853A4] p-3 lg:py-[17px] lg:px-[50px] text-white text-[15px] lg:text-[20px] font-[500] rounded-[5px]"
               >
-                Register
+                Edit User
               </button>
             </div>
           </form>
